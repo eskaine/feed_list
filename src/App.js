08b04ce1ef4c3606c4cfb31b1e189493c8c0cb78
eskaine <eskaineliu@gmail.com>
2021-01-25 15:32:51 +0800

    implement data fetching

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [feeds, setFeeds] = useState([]);
+
+  const getFeeds = () => {
+    const { REACT_APP_API, REACT_APP_KEY } = process.env;
+
+    fetch(`${REACT_APP_API}/?client_id=${REACT_APP_KEY}`)
+      .then((res) => res.json())
+      .then((data) => setFeeds(data))
+      .catch((error) => new Error(error));
+  };
+
+  useEffect(() => {
+    getFeeds();
+  }, []);
+
   return <div></div>;
 }
 
